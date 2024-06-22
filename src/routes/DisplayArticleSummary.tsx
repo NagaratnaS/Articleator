94816dc4a6c9b77c568d008121b8b7bca10b7e66
Nagaratna Hegde <nagaratnahegde@Nagaratnas-MacBook-Air.local>
2024-06-22 19:04:09 +0530

    styling changes

--- src/routes/DisplayArticleSummary.tsx
+++ src/routes/DisplayArticleSummary.tsx
@@ -1,3 +1,4 @@
+import { Card } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
@@ -9,7 +10,7 @@ export default function DisplayArticleSummary() {
           try {
             const response = await fetch(`https://ps-dev-1-partnergateway.patientsky.dev/assignment/articles/${id}`);
     
-            if (!response.ok) { // Check if the response is successful
+            if (!response.ok) {
               throw new Error(`HTTP error! status: ${response.status}`);
             }
     
@@ -22,10 +23,19 @@ export default function DisplayArticleSummary() {
     
         fetchData();
       }, []); 
-      console.log(id);
     return(
-        <div>
-            <h3>{apiData["summary"]}</h3>
+        <div style={{margin: 'auto',width: '50%',padding: '10px', marginTop: '5%', textAlign: 'center'}}>
+            <Card variant="outlined" style={{
+                height: '100px',
+                width: '30%',
+                marginLeft: '20%',
+                textAlign: 'center', 
+                marginTop: '5%', 
+                padding: '15%',
+                background: 'radial-gradient(178.94% 106.41% at 26.42% 106.41%, #FFF7B1 0%, rgba(255, 255, 255, 0) 71.88%), #FFFFFF',
+                boxShadow: '0px 155px 62px rgba(0, 0, 0, 0.01), 0px 87px 52px rgba(0, 0, 0, 0.05), 0px 39px 39px rgba(0, 0, 0, 0.09), 0px 10px 21px rgba(0, 0, 0, 0.1), 0px 0px 0px rgba(0, 0, 0, 0.1)',
+            }}>{apiData["summary"]}</Card>
         </div>
+        
     );
 }
