70b94995a7bf73a018dcfdbb64ba47ea1f26b3df
Nagaratna Hegde <nagaratnahegde@Nagaratnas-MacBook-Air.local>
2024-06-23 12:44:15 +0530

    routing url fixes

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Router, Route, BrowserRouter, Routes, useParams } from 'react-router-dom';
+import { Router, Route, BrowserRouter, Routes, useParams, Navigate } from 'react-router-dom';
 import ArticleApi from './api/ArticleApi';
 import DisplayArticleSummary from './routes/DisplayArticleSummary';
 
@@ -13,7 +13,8 @@ root.render(
     <h3 style= {{marginLeft: '45%',marginTop: '0', marginBottom: '3%'}}>Articleator</h3>
     <BrowserRouter>
       <Routes>
-        <Route path="" element={<ArticleApi />} />
+      <Route path="/" element={<Navigate to="/article" replace />} />
+        <Route path="/article" element={<ArticleApi />} />
         <Route path="/article/:id" element = {<DisplayArticleSummary/>}/>
       </Routes>
     </BrowserRouter>
